94d4ac9f74d15a43bf608488ab32a41035c630f8
denisb <denisb@student.ethz.ch>
2024-03-07 16:52:43 +0100

    initial commit

---
+++ src/NAND_flash.h
@@ -0,0 +1,14 @@
+/*
+ * NAND_flash.h
+ *
+ * This header file defines a set of functions for interacting with a FAT file system
+ * using the Zephyr OS File System API. The functions abstract common file operations,
+ * including initialization of the file system, opening, reading, writing, and closing
+ * files, as well as manipulating directories. The aim is to provide a simplified interface
+ * for applications to perform file operations on a FAT file system..
+ *
+ *
+ *
+ * Author: [Denis Buckingham]
+ * Date: [10.03.2024]
+ */
---
+++ src/ftl_to fs_link.h
@@ -0,0 +1,8 @@
+/**
+ * @file dhara_fat_adapter.h
+ * @brief Adapter to integrate the Dhara NAND Flash Translation Layer (FTL) with the FAT file system in Zephyr RTOS.
+ *
+ * 
+ * Author: [Denis Buckingham]
+ * Date: [10.03.2024]
+ */
