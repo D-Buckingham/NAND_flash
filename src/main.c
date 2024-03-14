d38c5e133ebcab8eacf8022b0847df358840cd98
denisb <denisb@student.ethz.ch>
2024-03-14 15:58:39 +0100

    spi working

--- src/main.c
+++ src/main.c
@@ -3,8 +3,11 @@
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 
+#include <zephyr/drivers/gpio.h>                                                                                                                                                     
+#include <zephyr/drivers/spi.h>
 
-//#include "SPI_NAND_DHARA/spi_nand_oper.h"
+
+#include "SPI_NAND_DHARA/spi_nand_oper.h"
 
 LOG_MODULE_REGISTER(main);
 
@@ -13,13 +16,11 @@ LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
-	
-	LOG_DBG("Debug");
-	printk("just anything");
+
 	LOG_INF("My first breath as an IoT device");
 	//Test the SPI communication
-	//const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(arduino_spi));
-	//spi_nand_test(dev);
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(arduino_spi));
+	spi_nand_test(dev);
 
 }
 
