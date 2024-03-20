d12957f36518504d64616f2471fb48033e4a8c36
denisb <denisb@student.ethz.ch>
2024-03-20 09:11:46 +0100

    intermediate commit while debugging

--- src/main.c
+++ src/main.c
@@ -12,10 +12,28 @@
 
 LOG_MODULE_REGISTER(main);
 
+/*
+#define MY_SPI_MASTER DT_NODELABEL(spi4)
 
+struct spi_cs_control spim_cs = {
+	.gpio = SPI_CS_GPIOS_DT_SPEC_GET(DT_NODELABEL(reg_my_spi_master)),
+	.delay = 0,
+};
 
+static const struct spi_config spi_cfg = {
+	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB |
+				 SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_LINES_SINGLE,
+	.frequency = 6000000,
+	.slave = 0,
+	.cs = (struct spi_cs_control *) &spim_cs,
+};
+
+*/
 #define BUFFER_SIZE 4
 
+#define SPI_OP   SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8) | SPI_LINES_SINGLE
+
+/*
 static const struct spi_config spi_cfg = {
     .frequency = 6000000, // TODO adjust the frequency as necessary
     .operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8) | SPI_LINES_SINGLE,//test, should be correct, CPOL = 0, CPHA = 0
@@ -24,10 +42,10 @@ static const struct spi_config spi_cfg = {
         .gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(arduino_spi), cs_gpios),
     },
 };
-
+*/
 int main(void)
 {
-
+	/*
 	struct device *spi4nn = device_get_binding("spi4n");
     if (!spi4nn) {
         LOG_INF("Failed to find SPI device %s\n", "spidev");
@@ -38,13 +56,20 @@ int main(void)
         .frequency = 1625000U,
         .operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB | SPI_OP_MODE_MASTER,
     };
+	*/
 
-	const struct device *spi = DEVICE_DT_GET(DT_NODELABEL(spi4));
+	const struct spi_dt_spec spi_dev =
+                SPI_DT_SPEC_GET(DT_NODELABEL(reg_my_spi_master), SPI_OP, 0);
 
-	if (!device_is_ready(spi)) {
+	//const struct device *spi_dev = DEVICE_DT_GET(MY_SPI_MASTER);
+
+	if (!device_is_ready(&spi_dev)) {
         LOG_ERR("Device SPI not ready, aborting test");
-        
     }
+	/*
+	if(!device_is_ready(spim_cs.gpio.port)){
+		LOG_ERR("SPI master chip select device not ready!");
+	}*/
 
 	uint8_t tx_buffer[BUFFER_SIZE] = {0xAA, 0xBB, 0xCC, 0xDD};
     uint8_t rx_buffer[BUFFER_SIZE] = {0};
@@ -53,8 +78,11 @@ int main(void)
     struct spi_buf_set tx_bufs = {.buffers = &tx_buf, .count = 1};
     struct spi_buf_set rx_bufs = {.buffers = &rx_buf, .count = 1};
 
-    int err = spi_transceive(spi, &spi_cfg2, &tx_bufs, &rx_bufs);
-
+    
+	int error = spi_transceive_dt(&spi_dev, &tx_bufs, &rx_bufs);
+	if(error != 0){
+		LOG_ERR("SPI transceive error: %i", error);
+	}
 
 	/*
 	const struct device *spi_dev = device_get_binding(SPI_DEV);
@@ -64,14 +92,13 @@ int main(void)
     }
 */
 	
-    if (err) {
-        LOG_INF("SPI transceive failed with error %d\n", err);
+    if (error) {
+        LOG_INF("SPI transceive failed with error %d\n", error);
     } else {
         LOG_INF("Received: ");
         for (int i = 0; i < BUFFER_SIZE; i++) {
             LOG_INF("0x%X ", rx_buffer[i]);
         }
-        LOG_INF("\n");
     }
 
 	LOG_INF("My first breath as an IoT device");
