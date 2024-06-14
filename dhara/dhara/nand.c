4690166951e3444e9e6ef8b5e566ce82d5ac42fe
denisb <denisb@student.ethz.ch>
2024-06-14 14:52:44 +0200

    now it seems to preserve the counter

--- dhara/dhara/nand.c
+++ dhara/dhara/nand.c
@@ -40,6 +40,10 @@ size_t NEW_ECC_counter = 0;
 size_t Delta_ECC_counter = 0;
 size_t Initial_ECC_counter = 0;
 
+
+uint32_t erase_count_indicator = 0;
+uint32_t total_ecc_counter = 0;
+
 //defined in 
 
 /** @brief waiting for finished transaction
@@ -239,9 +243,45 @@ int dhara_nand_erase(const struct dhara_nand *n, dhara_block_t b, dhara_error_t 
     LOG_INF("erase_block, block=%u", b);
     struct spi_nand_flash_device_t *dev = CONTAINER_OF(n, struct spi_nand_flash_device_t, dhara_nand);
     int ret;
+    uint32_t ecc_count_indicator = 0;
 
     dhara_page_t first_block_page = b * (1 << n->log2_ppb);
     uint8_t status;
+
+
+    //first read out the flags and store them, they will be written to the page
+    ret = read_page_and_wait(dev, first_block_page, NULL);//Initiate page read operation to cache. Reads the specified page from NAND to the device's internal cache.
+    if (ret) {
+        LOG_ERR("Failed to read page %u", first_block_page);
+        return -1;
+    }
+
+    // Read the current erase count indicator from the spare area
+    ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&erase_count_indicator, dev->page_size + ERASE_COUNTER_SPARE_AREA_OFFSET, 4);
+    if (ret != 0) {
+        LOG_ERR("Failed to read erase count from spare area: %d", ret);
+        return ret;
+    }
+
+    LOG_INF("Current erase count for block at page %u: %u", Erased_block, erase_count_indicator);
+
+    // Increment the erase count
+    erase_count_indicator++;
+    if (erase_count_indicator == 0){erase_count_indicator++;}
+
+
+    //read out the ECC counter from the first page of each block
+    ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&ecc_count_indicator, dev->page_size + ECC_COUNTER_SPARE_AREA_OFFSET, 4);
+    if (ret != 0) {
+        LOG_ERR("Failed to read erase count from spare area: %d", ret);
+        return ret;
+    }
+    if(Initial_ECC_counter == 0){Initial_ECC_counter = ecc_count_indicator;}//Initializing counter
+    if(ecc_count_indicator > Initial_ECC_counter + Delta_ECC_counter){//we found a counter value, larger than the one we locally stored since the last start up
+        Initial_ECC_counter = ecc_count_indicator;//thus adjusting the Initial one
+    }
+    total_ecc_counter = Initial_ECC_counter + Delta_ECC_counter;
+
 
     ret = spi_nand_write_enable(dev->config.spi_dev);
     if (ret != 0) {
@@ -292,9 +332,7 @@ int dhara_nand_prog(const struct dhara_nand *n, dhara_page_t p, const uint8_t *d
     int ret;
     uint8_t status;
     uint16_t used_marker = 0;
-    uint32_t erase_count_indicator = 0;
-    uint32_t ecc_count_indicator = 0;
-    uint32_t total_ecc_counter = 0;
+    
 
     ret = read_page_and_wait(dev, p, NULL);//Initiate page read operation to cache. Reads the specified page from NAND to the device's internal cache.
     if (ret) {
@@ -304,35 +342,35 @@ int dhara_nand_prog(const struct dhara_nand *n, dhara_page_t p, const uint8_t *d
 
 
 
-    //check if the erase counter has to be increased as well
-    if(Erase_counter_FLAG && Erased_block == p){//is first page of block
+    // //check if the erase counter has to be increased as well
+    // if(Erase_counter_FLAG && Erased_block == p){//is first page of block
 
-        // Read the current erase count indicator from the spare area
-        ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&erase_count_indicator, dev->page_size + ERASE_COUNTER_SPARE_AREA_OFFSET, 4);
-        if (ret != 0) {
-            LOG_ERR("Failed to read erase count from spare area: %d", ret);
-            return ret;
-        }
+    //     // Read the current erase count indicator from the spare area
+    //     ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&erase_count_indicator, dev->page_size + ERASE_COUNTER_SPARE_AREA_OFFSET, 4);
+    //     if (ret != 0) {
+    //         LOG_ERR("Failed to read erase count from spare area: %d", ret);
+    //         return ret;
+    //     }
 
-        LOG_INF("Current erase count for block at page %u: %u", Erased_block, erase_count_indicator);
+    //     LOG_INF("Current erase count for block at page %u: %u", Erased_block, erase_count_indicator);
 
-        // Increment the erase count
-        erase_count_indicator++;
-        if (erase_count_indicator == 0){erase_count_indicator++;}
+    //     // Increment the erase count
+    //     erase_count_indicator++;
+    //     if (erase_count_indicator == 0){erase_count_indicator++;}
 
 
-        //read out the ECC counter from the first page of each block
-        ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&ecc_count_indicator, dev->page_size + ECC_COUNTER_SPARE_AREA_OFFSET, 4);
-        if (ret != 0) {
-            LOG_ERR("Failed to read erase count from spare area: %d", ret);
-            return ret;
-        }
-        if(Initial_ECC_counter == 0){Initial_ECC_counter = ecc_count_indicator;}//Initializing counter
-        if(ecc_count_indicator > Initial_ECC_counter + Delta_ECC_counter){//we found a counter value, larger than the one we locally stored since the last start up
-            Initial_ECC_counter = ecc_count_indicator;//thus adjusting the Initial one
-        }
-        total_ecc_counter = Initial_ECC_counter + Delta_ECC_counter;
-    }
+    //     //read out the ECC counter from the first page of each block
+    //     ret = spi_nand_read(dev->config.spi_dev, (uint8_t *)&ecc_count_indicator, dev->page_size + ECC_COUNTER_SPARE_AREA_OFFSET, 4);
+    //     if (ret != 0) {
+    //         LOG_ERR("Failed to read erase count from spare area: %d", ret);
+    //         return ret;
+    //     }
+    //     if(Initial_ECC_counter == 0){Initial_ECC_counter = ecc_count_indicator;}//Initializing counter
+    //     if(ecc_count_indicator > Initial_ECC_counter + Delta_ECC_counter){//we found a counter value, larger than the one we locally stored since the last start up
+    //         Initial_ECC_counter = ecc_count_indicator;//thus adjusting the Initial one
+    //     }
+    //     total_ecc_counter = Initial_ECC_counter + Delta_ECC_counter;
+    // }
 
     ret = spi_nand_write_enable(dev->config.spi_dev);//Enable writing on the SPI NAND device.
     if (ret) {
